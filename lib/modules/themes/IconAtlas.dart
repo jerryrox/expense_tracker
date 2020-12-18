594be6abdc5612878340dd3596722f05585971c0
Jerry Kim <jerryrox@live.co.kr>
2020-12-18 16:32:54 +1300

    Create IconAtlas.dart

---
+++ lib/modules/themes/IconAtlas.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+/// A class which provides the list of icon data used in the app.
+class IconAtlas {
+
+  static final warning = Icons.warning;
+
+  IconAtlas._();
+}
