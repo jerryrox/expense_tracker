c5caa3e20e8a40cfac463c9eee82034ef8e7d815
Jerry Kim <jerryrox@live.co.kr>
2020-12-18 20:20:46 +1300

    Removed api provider as it was unnecessary.

--- lib/modules/dependencies/DependencyContainer.dart
+++ lib/modules/dependencies/DependencyContainer.dart
@@ -1,4 +1,3 @@
-import 'package:expense_tracker/modules/dependencies/ApiProvider.dart';
 import 'package:expense_tracker/modules/dependencies/ScreenManager.dart';
 import 'package:expense_tracker/modules/dependencies/states/UserState.dart';
 import 'package:provider/provider.dart';
@@ -9,19 +8,16 @@ class DependencyContainer {
   UserState userState;
 
   ScreenManager screenManager;
-  ApiProvider apiProvider;
 
   DependencyContainer() {
     userState = UserState();
 
     screenManager = ScreenManager();
-    apiProvider = ApiProvider();
   }
 
   /// Returns the dependencies as list of providers.
   List<SingleChildWidget> getProviders() {
     return [
-        Provider.value(value: apiProvider),
         Provider.value(value: userState),
         Provider.value(value: screenManager),
     ];
