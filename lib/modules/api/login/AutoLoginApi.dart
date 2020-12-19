af424b397f9a63e7e8cf45411216deeacb28f191
Jerry Kim <jerryrox@live.co.kr>
2020-12-19 16:04:01 +1300

    Fixed bug and added some doc style comments

--- lib/modules/api/login/AutoLoginApi.dart
+++ lib/modules/api/login/AutoLoginApi.dart
@@ -4,7 +4,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AutoLoginApi extends BaseLoginApi {
 
   @override
-  Future<User> request() {
-    return Future.sync(() => FirebaseAuth.instance.currentUser);
+  Future<User> request() async {
+    for(int i=0; i<4; i++) {
+      await Future.delayed(Duration(milliseconds: 500));
+      final user = FirebaseAuth.instance.currentUser;
+      if(user != null) {
+        return user;
+      }
+    }
+    return null;
   }
 }
