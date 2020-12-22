ad51aaad81a3726c2571ac51396fa1c1bb5e9494
Jerry Kim <jerryrox@live.co.kr>
2020-12-22 15:38:55 +1300

    Implemented categories list in HomeScreen

--- lib/ui/navigations/screens/HomeScreen.dart
+++ lib/ui/navigations/screens/HomeScreen.dart
@@ -1,9 +1,7 @@
 import 'package:expense_tracker/modules/models/Category.dart';
 import 'package:expense_tracker/modules/models/ExpenseChartData.dart';
 import 'package:expense_tracker/modules/models/Record.dart';
-import 'package:expense_tracker/modules/models/RecordContext.dart';
 import 'package:expense_tracker/modules/models/RecordGroup.dart';
-import 'package:expense_tracker/modules/models/static/ColorUtils.dart';
 import 'package:expense_tracker/modules/themes/IconAtlas.dart';
 import 'package:expense_tracker/modules/types/DateRangeType.dart';
 import 'package:expense_tracker/ui/components/primitives/ButtonGroup.dart';
@@ -12,6 +10,7 @@ import 'package:expense_tracker/ui/components/primitives/ExpenseChart.dart';
 import 'package:expense_tracker/ui/components/primitives/FilledBox.dart';
 import 'package:expense_tracker/ui/components/primitives/RoundedButton.dart';
 import 'package:expense_tracker/ui/components/primitives/TitleText.dart';
+import 'package:expense_tracker/ui/components/screens/home/CategoryListDisplay.dart';
 import 'package:expense_tracker/ui/components/screens/home/TotalSpentDisplay.dart';
 import 'package:flutter/material.dart';
 
@@ -32,14 +31,18 @@ class _HomeScreenState extends State<HomeScreen> {
 
   /// Shows the record page to start recording a new item.
   void showRecordPage() {
-    // TODO:
+    // TODO: Navigate to RecordPage.
   }
 
-  /// Returns the data for the chart to display.
-  List<ExpenseChartData> getChartData() {
-    // TODO: Use actual data.
+  /// Shows the detail screen using the specified record group.
+  void showDetailScreen(RecordGroup recordGroup) {
+    // TODO: Navigate to DetailScreen. Pass the category and date range type.
+  }
 
-    List<RecordGroup> records = [
+  /// Returns the list of record groups evaluated from current state.
+  List<RecordGroup> getRecordGroups() {
+    // TODO: Use actual data.
+    return [
       RecordGroup(
         category: Category(id: "a", name: "Food", color: 0xffff8888),
         records: [
@@ -57,13 +60,13 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       )
     ];
-    return records.map((e) {
+  }
+
+  /// Returns the data for the chart to display.
+  List<ExpenseChartData> getChartData() {
+    return getRecordGroups().map((e) {
       Color color = Color(e.category.color);
-      return ExpenseChartData(
-        color: color,
-        label: e.category.name,
-        value: e.totalAmount
-      );
+      return ExpenseChartData(color: color, label: e.category.name, value: e.totalAmount);
     }).toList();
   }
 
@@ -97,11 +100,19 @@ class _HomeScreenState extends State<HomeScreen> {
                         // TODO: Use real amount
                         amount: 0,
                       ),
-                      SizedBox(height: 30),
                       ConstrainedBox(
-                        constraints: BoxConstraints(maxWidth: 300),
+                        constraints: BoxConstraints(maxHeight: 300, maxWidth: 400),
                         child: ExpenseChart(
                           data: getChartData(),
+                        ),
+                      ),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(
+                          maxWidth: 400,
+                        ),
+                        child: CategoryListDisplay(
+                          recordGroups: getRecordGroups(),
+                          onClick: _onCategoryButton,
                         ),
                       ),
                     ],
@@ -145,5 +156,10 @@ class _HomeScreenState extends State<HomeScreen> {
   /// Event called on record new button.
   void _onRecordButton() {
     showRecordPage();
+  }
+
+  /// Event called on category button click.
+  void _onCategoryButton(RecordGroup recordGroup) {
+    showDetailScreen(recordGroup);
   }
 }
